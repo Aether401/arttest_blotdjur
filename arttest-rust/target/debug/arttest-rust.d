98f7934a4f68fe1aef7acacb51ecaa694ab09e3a
FirePuma142 <mans.ekenberg@icloud.com>
2024-06-21 23:31:16 +0200

    Added files

---
+++ arttest-rust/target/debug/arttest-rust.d
@@ -0,0 +1,1 @@
+/Users/mansfalk/Code/arttest_blotdjur/arttest-rust/target/debug/arttest-rust: /Users/mansfalk/Code/arttest_blotdjur/arttest-rust/src/main.rs
---
+++ arttest-rust/target/debug/deps/arttest_rust-6a12b785fb8e4b67.d
@@ -0,0 +1,5 @@
+/Users/mansfalk/Code/arttest_blotdjur/arttest-rust/target/debug/deps/arttest_rust-6a12b785fb8e4b67: src/main.rs
+
+/Users/mansfalk/Code/arttest_blotdjur/arttest-rust/target/debug/deps/arttest_rust-6a12b785fb8e4b67.d: src/main.rs
+
+src/main.rs:
